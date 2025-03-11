e975861f71da861d8a9455fe4e049a968fbd963d
Nikhil13k <karankotnikhilkumar@gmail.com>
2025-03-11 14:09:56 +0530

    Initial Commit

---
+++ Cart/src/test/java/com/nkk/Cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkk.Cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
